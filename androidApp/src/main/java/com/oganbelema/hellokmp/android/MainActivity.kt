797a1c29376b8d6b1adda45061da36da463b91a0
Oganbelema <oganbelema@gmail.com>
2022-09-09 15:01:05 +0100

    added network request functionality and displayed the result

--- androidApp/src/main/java/com/oganbelema/hellokmp/android/MainActivity.kt
+++ androidApp/src/main/java/com/oganbelema/hellokmp/android/MainActivity.kt
@@ -4,17 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oganbelema.hellokmp.Greeting
 import android.widget.TextView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
-fun greet(): String {
+suspend fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        tv.text = "Loading..."
+
+        scope.launch {
+            kotlin.runCatching {
+                greet()
+            }.onSuccess {
+                tv.text = it
+            }.onFailure {
+                tv.text = it.localizedMessage
+            }
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
     }
 }
--- shared/src/commonMain/kotlin/com/oganbelema/hellokmp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/oganbelema/hellokmp/Greeting.kt
@@ -1,10 +1,36 @@
 package com.oganbelema.hellokmp
 
+import RocketLaunch
 import daysUntilNewYear
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 
 class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform.reversed()}!" +
-                "\nThere are only ${daysUntilNewYear()} days left until New Year!"
+
+    private val httpClient = HttpClient {
+        install(ContentNegotiation){
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    @Throws(Exception::class)
+    suspend fun greeting(): String {
+        val rockets: List<RocketLaunch> = httpClient
+            .get("https://api.spacexdata.com/v4/launches")
+            .body()
+
+        val lastSuccessLaunch = rockets.last { it.launchSuccess == true }
+
+        return "Guess what it is! > ${Platform().platform.reversed()}!" +
+                "\nThere are only ${daysUntilNewYear()} left until New Year! 🎅🏼 " +
+                "\nThe last successful launch was ${lastSuccessLaunch.launchDateUTC} 🚀"
     }
 }
