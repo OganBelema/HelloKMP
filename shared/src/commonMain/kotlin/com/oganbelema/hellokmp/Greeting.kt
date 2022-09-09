f6c6bd720ea2bbb4381332eed97cc723d168c75d
Oganbelema <oganbelema@gmail.com>
2022-09-09 11:03:34 +0100

    modified greeting function to display the platform in reverse

--- shared/src/commonMain/kotlin/com/oganbelema/hellokmp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/oganbelema/hellokmp/Greeting.kt
@@ -2,6 +2,6 @@ package com.oganbelema.hellokmp
 
 class Greeting {
     fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
+        return "Hello, ${Platform().platform.reversed()}!"
     }
 }
