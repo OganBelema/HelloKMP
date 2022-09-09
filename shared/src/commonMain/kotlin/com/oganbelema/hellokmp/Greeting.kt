93949912776eabd6dedb40da58bdb8b0d4c52d9e
Oganbelema <oganbelema@gmail.com>
2022-09-09 11:26:05 +0100

    added a new dependency for common sourceset and updated greeting to call a function to display days unitl new yaer

--- shared/src/commonMain/kotlin/com/oganbelema/hellokmp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/oganbelema/hellokmp/Greeting.kt
@@ -1,7 +1,10 @@
 package com.oganbelema.hellokmp
 
+import daysUntilNewYear
+
 class Greeting {
     fun greeting(): String {
-        return "Hello, ${Platform().platform.reversed()}!"
+        return "Hello, ${Platform().platform.reversed()}!" +
+                "\nThere are only ${daysUntilNewYear()} days left until New Year!"
     }
 }
